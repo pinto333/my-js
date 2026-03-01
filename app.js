66e34a8070725f7da00d34be513fae041994e8fb
Your GitHub Username <alonadi@MacBook-Pro-sl-Alon.local>
2026-03-01 16:14:57 +0200

    Update app.js with new logic

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+// הגדרת משתנים
+const name = "אלון";
+const hoursInDay = 24;
+const daysInYear = 365;
+
+// חישוב פשוט
+const totalHoursAYear = hoursInDay * daysInYear;
+
+console.log("שלום " + name + "!");
+console.log("האם ידעת שבשנה אחת יש " + totalHoursAYear + " שעות?");
